0681b6e18b56c09a7917eb349355651d6fc5219d
ksukriti08 <ksukriti08@gmail.com>
2025-03-06 23:58:41 -0500

    updates

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'goal_pose_filter = goal_pose_filter.goal_pose_filter_node:main',  # Add this line
         ],
     },
 )
